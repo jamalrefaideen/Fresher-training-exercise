b7e8b22afe7345200b9a401894c4f75c75931b9d
Bhuvanesh <bhuvaneshpandian14@gmail.com>
2020-06-18 10:33:01 +0530

    day2 task completed

---
+++ Bhuvanesh/day2/even.js
@@ -0,0 +1,6 @@
+
+var i;
+for (i = 0; i < 20; i++)
+if ((i % 2) == 0)
+  document.writeln(i);
+
---
+++ Bhuvanesh/day2/mul.js
@@ -0,0 +1,9 @@
+var nums = ['0','1','2','3','4','5','6','7','8','9','10'];
+mul=9;
+
+for(var i=0; i<=10; i++){
+
+        var num = nums[i] * mul;
+
+        document.writeln(num);
+}
