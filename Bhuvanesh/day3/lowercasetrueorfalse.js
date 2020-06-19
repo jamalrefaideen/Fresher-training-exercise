2a6e3b08b7cf3340466dc77089a92acf7831a954
Bhuvanesh <bhuvaneshpandian14@gmail.com>
2020-06-19 12:20:31 +0530

    day03 task completed

---
+++ Bhuvanesh/day3/lowercasetrueorfalse.js
@@ -0,0 +1,11 @@
+
+         function compareStr() {
+            var string1 = "BHUVANESH";
+            var string2 = "bhuvanesh;
+            var result = string1.localeCompare(string2);
+
+            console.log( result +"false");
+
+         }
+   compareStr();
+      
