181c558bcbab005ce6f262dd19e21666ea0e7e0b
Bhuvanesh <bhuvaneshpandian14@gmail.com>
2020-06-22 12:58:56 +0530

    day4 task completed

---
+++ Bhuvanesh/day4/find first element.js
@@ -0,0 +1,10 @@
+function getFirstElement(){
+var name=["Bhuvanesh","Pandian","Arun","Chandru"]
+var string="";
+var i;
+for(i=0;i<=name.length;i++){
+    string=name[0];
+    console.log(string )
+}
+}
+getFirstElement()
---
+++ Bhuvanesh/day4/join all element.js
@@ -0,0 +1,9 @@
+//join all element
+function joinString(){
+var myColor=["Red","Green","White","Black"];
+console.log(myColor.join(  " , "));
+console.log(myColor.join(  " , "));
+console.log(myColor.join(  " + "));
+}
+
+joinString()
