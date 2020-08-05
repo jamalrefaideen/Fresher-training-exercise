466ff78134085fb5f5651a7b0fcbda90e794a5a1
Buhati Sahith <buharisahith@gmail.com>
2020-08-05 11:22:26 +0530

    Task Completed

--- Buhari/Day 24/form2.js
+++ Buhari/Day 24/form2.js
@@ -1,55 +1,69 @@
+var form = document.querySelector("form");
+var employeeId = document.querySelector("#empId");
+var name = document.querySelector("#name");
+var  address = document.querySelector("#address");
+var submitBtn = document.querySelector(".submitbtn");
+var updateBtn = document.querySelector(".updatebtn")
+var tbody = document.querySelector("tbody");
+
 function UserDetails (employeeId, name , address, ){
-    this.employeeId = employeeId;
-    this.name = name ;
+    this.employeeId = employeeId
+    this.name = name 
     this.address = address
 }
 function Ui(){
-    
+}
+Ui.addUser = function (user){
+  var tr = document.createElement('tr');
+  tr.innerHTML=`
+  <td>${user.employeeId}</td>
+  <td>${user.name}</td>
+  <td>${user.address}</td>
+  <td><button type ="button" id="edit-btn">Edit</button></td>
+  <td><button type ="button" id="delete-btn">Delete</button></td>`;
+tbody.appendChild(tr);
+}
+Ui.clearFields = function (){
+  employeeId.value ="";
+  name.value="";
+  address.value="";
+}
+submitBtn.addEventListener("click", userDetails)
+function userDetails(event){    
+  var employeeId = employeeId.value;
+  var name = name.value;
+  var address = address.value;
+var UserInfo = new User(employeeId,name,address);
+Ui.addUser(UserInfo)
+Ui.clearFields();
 }
 
-function getInformations(){
-event.preventDefault();
-var userInformation = {
-    employeeId: event.target.form[0].value,
-    name: event.target.form[1].value,
-    address: event.target.form[2].value,
-    
-}
-    showUserDetails() {
-            if (event.target.id == "edit-btn") {
-              var employeeId = event.target.parentElement.parentElement.cells.item(0)
-                .textContent;
-              editUserField(employeeId);
-            } else if (event.target.id == "delete-btn") {
-              var employeeId = event.target.parentElement.parentElement.cells.item(0)
-                .textContent;
-              deleteField(employeeId);
-              event.target.parentElement.parentElement.remove();
-            }
-          }
-    };
+//     showUserDetails() {
+//             if (event.target.id == "edit-btn") {
+//               var employeeId = event.target.parentElement.parentElement.cells.item(0)
+//                 .textContent;
+//               editUserField(employeeId);
+//             } else if (event.target.id == "delete-btn") {
+//               var employeeId = event.target.parentElement.parentElement.cells.item(0)
+//                 .textContent;
+//               deleteField(employeeId);
+//               event.target.parentElement.parentElement.remove();
+//             }
+//           }
+//     };
 
-    deleteUserDetails() {
+//     deleteUserDetails() {
 
-    };
+//     };
 
-    editUserDetails(){
+//     editUserDetails(){
 
-    };
+//     };
 
-    updateUserDetails(){
+//     updateUserDetails(){
 
-    };
-}
-submitBtn.addEventListener("click", UserDetails)
-function UserDetails(){    
-var employeeId = document.querySelector("#empid");
-var Name = document.querySelector("#name");
-var address = document.querySelector("#address");
-var Ui = new Ui(userInformation)
-Ui.showUserDetails(UserDetails)
-}
-
+//     };
+// }
 
 
     
